bfde7cba47e3119ac7d361a787db52f7c3c75159
Poulami Saha <saha.poulami91@gmail.com>
2024-02-25 07:46:36 +0100

    Correction done

--- basic-challenges-1/palindrome/palindrome.ts
+++ basic-challenges-1/palindrome/palindrome.ts
@@ -1,6 +1,6 @@
 const palindrome = (str) => {
     const formattedString = str.toLowerCase().replace(/[^a-z0-9]/g, "")
-    const reversed = str.split("").reverse().join("");
+    const reversed = formattedString.split("").reverse().join("");
     return reversed === formattedString;
 }
 
